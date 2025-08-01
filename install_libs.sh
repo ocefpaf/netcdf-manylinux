35e29a405d74de914bfb9427bf4883eb9d4ced26
Filipe Fernandes <ocefpaf@gmail.com>
2025-08-01 11:37:26 -0300

    native aarch, zizmore fixes, GHA updates

--- install_libs.sh
+++ install_libs.sh
@@ -4,9 +4,31 @@ set -euo pipefail
 # yum -y install wget zlib-devel curl-devel
 yum -y install wget zlib-devel perl-IPC-Cmd
 
+build_openssl() {
+    wget https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_VERSION}/openssl-${OPENSSL_VERSION}.tar.gz
+    tar -xzvf openssl-${OPENSSL_VERSION}.tar.gz
+
+    pushd openssl-${OPENSSL_VERSION}
+      ./config shared -fPIC shared --prefix=${BUILD_PREFIX} --libdir=lib
+      make -j$(nproc)
+      make install_sw install_ssldirs
+    popd
+}
+
+build_curl() {
+    flags="--prefix=${BUILD_PREFIX} --disable-ldap --with-openssl=${BUILD_PREFIX} --without-zstd --without-libpsl"
+    wget https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
+    tar -xzvf curl-${CURL_VERSION}.tar.gz
+    pushd curl-${CURL_VERSION}
+      ./configure ${flags}
+      make -j$(nproc)
+      make install
+    popd
+}
+
 build_libaec(){
     # The URL includes a hash, so it needs to change if the version does
-    wget  https://gitlab.dkrz.de/k202009/libaec/uploads/45b10e42123edd26ab7b3ad92bcf7be2/libaec-${AEC_VERSION}.tar.gz
+    wget  https://gitlab.dkrz.de/-/project/117/uploads/dc5fc087b645866c14fa22320d91fb27/libaec-${AEC_VERSION}.tar.gz
     tar zxf libaec-${AEC_VERSION}.tar.gz
 
     echo "Building & installing libaec"
@@ -22,34 +44,14 @@ build_hdf5() {
     ldconfig
 
     #                           Remove trailing .*, to get e.g. 'major.minor' ↓
-    wget "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-${HDF5_VERSION}/src/hdf5-${HDF5_VERSION}.tar.gz"
-    tar -xzvf hdf5-${HDF5_VERSION}.tar.gz
-    pushd hdf5-${HDF5_VERSION}
+    HDF5_TAG="hdf5_${HDF5_VERSION}"
+    wget "https://github.com/HDFGroup/hdf5/archive/refs/tags/${HDF5_TAG}.tar.gz"
+    tar -xzvf hdf5_${HDF5_VERSION}.tar.gz
+    pushd hdf5-${HDF5_TAG}
       chmod u+x autogen.sh
 
       echo "Configuring, building & installing HDF5 ${HDF5_VERSION} to ${BUILD_PREFIX}"
       ./configure --prefix ${BUILD_PREFIX} --enable-build-mode=production --with-szlib
-      make -j$(nproc)
-      make install
-    popd
-}
-
-build_openssl() {
-    wget http://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
-    tar -xzvf openssl-${OPENSSL_VERSION}.tar.gz
-    pushd openssl-${OPENSSL_VERSION}
-      ./config shared -fPIC shared --prefix=${BUILD_PREFIX} --libdir=lib
-      make -j$(nproc)
-      make install
-    popd
-}
- 
-build_curl() {
-    flags="--prefix=${BUILD_PREFIX} --disable-ldap --with-ssl --without-zstd"
-    wget https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
-    tar -xzvf curl-${CURL_VERSION}.tar.gz
-    pushd curl-${CURL_VERSION}
-      ./configure ${flags}
       make -j$(nproc)
       make install
     popd
@@ -60,7 +62,7 @@ build_netcdf() {
     NETCDF_SRC=netcdf-c
     NETCDF_BLD=netcdf-build
 
-    # We are building from lastest b/c # it has fix for setting CURL path to find SSL certificates.
+    # We are building from test b/c # it has fix for setting CURL path to find SSL certificates.
     git clone https://github.com/Unidata/netcdf-c ${NETCDF_SRC}
     # git clone ${netcdf_url} -b ${NETCDF_VERSION} ${NETCDF_SRC}
 
@@ -86,7 +88,7 @@ clean_up(){
          hdf5-${HDF5_VERSION}.tar.gz \
          libaec-${AEC_VERSION}.tar.gz \
          ${NETCDF_SRC}
-  
+
   # Can't execute this with our own curl in the path.
   # yum -y erase wget zlib-devel perl-IPC-Cmd
 }
